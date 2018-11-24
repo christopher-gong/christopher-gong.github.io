37196ab65ee9c7d26b782353e7cdf64dc2659bcb
Chris Gong <christophergong@berkeley.edu>
2018-11-23 16:19:06 -0800

    floaters working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
+    $(".floater").hover(function() {
+        $(this).css("transform", $(this).css("transform") + " scale(1.1)");
+    }, function() {
+        $(this).css("transform", "translateY(" + $(window).scrollTop()/-10 + "px)");
+    });
+    $(window).scroll(function(){
+            console.log($(window).scrollTop());
+            $('.floater').css("transform", "translateY(" + $(window).scrollTop()/-10 + "px)");
+    });
     setTimeout(function(){
         $(".handwriting-video").fadeOut(4000);
     }, 2000);
