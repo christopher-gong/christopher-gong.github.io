2243c5c1c6408bf20b7efd53a74aec88d45f78bd
Chris Gong <christophergong@berkeley.edu>
2018-11-23 17:10:05 -0800

    different speed floaties

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,17 @@ $(document).ready(function() {
     $(".floater").hover(function() {
         $(this).css("transform", $(this).css("transform") + " scale(1.1)");
     }, function() {
-        $(this).css("transform", "translateY(" + $(window).scrollTop()/-10 + "px)");
+        $('#f1').css("transform", "translateY(" + $(window).scrollTop()/-10 + "px)");
+        $('#f2').css("transform", "translateY(" + $(window).scrollTop()/-15 + "px)");
+        $('#f3').css("transform", "translateY(" + $(window).scrollTop()/-5 + "px)");
+        $('#f4').css("transform", "translateY(" + $(window).scrollTop()/-10 + "px)");
     });
     $(window).scroll(function(){
             console.log($(window).scrollTop());
-            $('.floater').css("transform", "translateY(" + $(window).scrollTop()/-10 + "px)");
+            $('#f1').css("transform", "translateY(" + $(window).scrollTop()/-10 + "px)");
+            $('#f2').css("transform", "translateY(" + $(window).scrollTop()/-15 + "px)");
+            $('#f3').css("transform", "translateY(" + $(window).scrollTop()/-5 + "px)");
+            $('#f4').css("transform", "translateY(" + $(window).scrollTop()/-10 + "px)");
     });
     setTimeout(function(){
         $(".handwriting-video").fadeOut(4000);
