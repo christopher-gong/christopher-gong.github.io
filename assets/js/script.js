bb11df0dd1ad9575da546f91621791dacd683d70
Chris Gong <christophergong@berkeley.edu>
2018-11-28 15:23:53 -0800

    done to turn in

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,21 @@
 $(document).ready(function() {
+
+    $(".down").on('click', function(event) {
+        window.scrollTo(0, document.body.scrollHeight / 3);
+      });
+
     $(".floater").hover(function() {
         $(this).css("transform", $(this).css("transform") + " scale(1.1)");
     }, function() {
-        $('#f1').css("transform", "translateY(" + $(window).scrollTop()/-10 + "px)");
-        $('#f2').css("transform", "translateY(" + $(window).scrollTop()/-15 + "px)");
+        $('#f1').css("transform", "translateY(" + $(window).scrollTop()/-9 + "px)");
+        $('#f2').css("transform", "translateY(" + $(window).scrollTop()/-8 + "px)");
         $('#f3').css("transform", "translateY(" + $(window).scrollTop()/-5 + "px)");
         $('#f4').css("transform", "translateY(" + $(window).scrollTop()/-10 + "px)");
     });
     $(window).scroll(function(){
             console.log($(window).scrollTop());
-            $('#f1').css("transform", "translateY(" + $(window).scrollTop()/-10 + "px)");
-            $('#f2').css("transform", "translateY(" + $(window).scrollTop()/-15 + "px)");
+            $('#f1').css("transform", "translateY(" + $(window).scrollTop()/-9 + "px)");
+            $('#f2').css("transform", "translateY(" + $(window).scrollTop()/-8 + "px)");
             $('#f3').css("transform", "translateY(" + $(window).scrollTop()/-5 + "px)");
             $('#f4').css("transform", "translateY(" + $(window).scrollTop()/-10 + "px)");
     });
